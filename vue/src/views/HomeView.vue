b5c325f1e22edd081967c13690985faf620ad63e
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-05-11 10:02:18 -0400

    access supabase client

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  'https://wuyfukyjgcpffyzwslge.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1eWZ1a3lqZ2NwZmZ5endzbGdlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzIwNzkwNiwiZXhwIjoxOTk4NzgzOTA2fQ.hmCBj52q58hRA7Tr5LQTqoBkFUsVar--Nfvh583EfPQ'
+)
+console.log(supabase)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
