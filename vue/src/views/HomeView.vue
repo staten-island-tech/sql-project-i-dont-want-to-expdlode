ad86f483c4eb29eea6fbe6355df069f831436448
Firdavsy07 <firdavsmuqimtoshov@gmail.com>
2023-05-15 09:31:48 -0400

    pinia

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,13 +1,10 @@
 <script>
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  'https://wuyfukyjgcpffyzwslge.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1eWZ1a3lqZ2NwZmZ5endzbGdlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzIwNzkwNiwiZXhwIjoxOTk4NzgzOTA2fQ.hmCBj52q58hRA7Tr5LQTqoBkFUsVar--Nfvh583EfPQ'
-)
-console.log(supabase)
+import { defineStore } from 'pinia'
+export const useMovieStore = defineStore('movies', {})
 </script>
+//view = declarative rendering of the state, getters = computed properties , //actions = asynchronus
+operations and ways to change the state,//state = what is stored to influence the view //State
+management takes the same slot as components//
 
 <template>
   <main></main>
